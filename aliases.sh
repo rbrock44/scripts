f9d5fd3729610d052c541e8846ea2d6c5004f383
rbrock44 <rbrock444@gmail.com>
2023-08-29 17:42:58 -0500

    add common aliases

---
+++ aliases.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# add these to the .bashrc file
+alias work='cd /c/workspace/'
+alias g=git
+alias gpull='git pull'
+alias gpush='git push'
+alias gpushn='git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)'
+alias gc='git checkout'
+alias ga='git add .'
+alias gcm='git commit -m'
