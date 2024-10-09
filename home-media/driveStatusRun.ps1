6f73607c6ee75962e1deca32a3f3186dedcf9d6a
rbrock44 <rbrock444@gmail.com>
2024-10-09 11:54:55 -0500

    add drive status run

---
+++ home-media/driveStatusRun.ps1
@@ -0,0 +1,21 @@
+Set-Location -Path 'C:\workspace\drive-status'
+if (-not (Test-Path .)) { exit }
+
+git stash
+git checkout master
+git pull
+
+Set-Location -Path 'C:\workspace\scripts'
+if (-not (Test-Path .)) { exit }
+
+git pull
+cd home-page
+.\smbConnectionResults.ps1
+
+Set-Location -Path 'C:\workspace\drive-status'
+if (-not (Test-Path .)) { exit }
+
+git add .
+$today = Get-Date -Format "yyyy/MM/dd HH:mm"
+git commit -m "drive status from $today"
+git push
