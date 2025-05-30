073ce7bcd58983e55c6ccf40841a54b3b7c6222d
R Brock <rbrock444@gmail.com>
2025-05-30 10:04:38 -0500

    Update check-smb.sh

--- s01/check-smb.sh
+++ s01/check-smb.sh
@@ -1,15 +1,15 @@
 declare -A shares=(
-  ["Share1"]="//10.0.0.50/usbc"
-  ["Share2"]="//10.0.0.50/usbd"
-  ["Share3"]="//10.0.0.50/usbe"
-  ["Share4"]="//10.0.0.50/usbf"
+  ["usbc"]="//10.0.0.50/usbc"
+  ["usbd"]="//10.0.0.50/usbd"
+  ["usbe"]="//10.0.0.50/usbe"
+  ["usbf"]="//10.0.0.50/usbf"
 )
 
 declare -A pushUrls=(
-  ["Share1"]="http://10.0.0.150:3002/api/push/wa2OJhU5O3?status=up&msg=OK&ping="
-  ["Share2"]="http://10.0.0.150:3002/api/push/sNXeytIaFv?status=up&msg=OK&ping="
-  ["Share3"]="http://10.0.0.150:3002/api/push/Ww4943DDeU?status=up&msg=OK&ping="
-  ["Share4"]="http://10.0.0.150:3002/api/push/N2Z0YLfMUv?status=up&msg=OK&ping="
+  ["usbc"]="http://10.0.0.150:3002/api/push/wa2OJhU5O3?status=up&msg=OK&ping="
+  ["usbd"]="http://10.0.0.150:3002/api/push/sNXeytIaFv?status=up&msg=OK&ping="
+  ["usbe"]="http://10.0.0.150:3002/api/push/Ww4943DDeU?status=up&msg=OK&ping="
+  ["usbf"]="http://10.0.0.150:3002/api/push/N2Z0YLfMUv?status=up&msg=OK&ping="
 )
 
 for name in "${!shares[@]}"; do
